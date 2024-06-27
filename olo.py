3f36c81583e2241a45011154e66a263dcec0535d
holaquesase <al.ampueroa@duocuc.cl>
2024-06-27 12:35:18 -0400

    Add files via upload

--- olo.py
+++ olo.py
@@ -1,1 +1,31 @@
-print("hola")
+def ReservarSalaConferencia():#alex
+    pass
+
+def ConsultarDisponibilidad():#akex
+    pass
+
+def AdministrarReservas():
+    pass
+
+def CargarReservas():
+    pass
+
+def GuardarReservas():
+    pass
+
+def menu():
+    print("menu 1 2 3 4 5")
+    op=int(input("pone tu caga"))
+    if op==1:
+        ReservarSalaConferencia()
+    elif op==2:
+        ConsultarDisponibilidad()
+    elif op==3:
+        AdministrarReservas()
+    elif op==4:
+        CargarReservas()
+    elif op==5:
+        GuardarReservas()
+    
+    pass
+menu()
